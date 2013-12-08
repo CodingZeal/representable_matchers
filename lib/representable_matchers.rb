009e75a67a226619318367d7ce91082dc0ae3e82
Adam Cuppy <adam@codingzeal.com>
2013-12-08 09:57:18 -0800

    remove WIP extension_of_matcher

--- lib/representable_matchers.rb
+++ lib/representable_matchers.rb
@@ -1,6 +1,5 @@
 require "matchers/version"
 require "matchers/class_name_matcher"
-require "matchers/extension_of_matcher"
 require "matchers/representable_property_matcher"
 require "matchers/have_representable_property_matcher"
 
