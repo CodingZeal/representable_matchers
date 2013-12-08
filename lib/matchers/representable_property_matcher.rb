e8a386827a1dc60d0d9ebd87761c4a9760d1787e
Adam Cuppy <adam@codingzeal.com>
2013-12-08 09:07:08 -0800

    representable property matcher

---
+++ lib/matchers/representable_property_matcher.rb
@@ -0,0 +1,34 @@
+module Representable
+  module Matchers
+
+    class RepresentablePropertyMatcher < Struct.new(:property)
+
+      def matches?(subject)
+        @subject = subject
+        representable_attribute?
+      end
+
+      def description
+        "only allow models with specific representable properties"
+      end
+
+      def failure_message_for_should
+        "Expected #{expectation}"
+      end
+
+      def failure_message_for_should_not
+        "Did not expect #{expectation}"
+      end
+
+      private
+
+      def representable_attribute?
+        !@subject.send(:representable_attrs)[property].nil?
+      end
+
+      def expectation
+        "#{@subject} to be a Representable property"
+      end
+    end
+  end
+end
